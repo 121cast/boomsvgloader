a37c8c0fe64a371b878dd7e3e7b85f318426d93d
Long Zheng <long@121cast.com>
2019-01-16 12:15:20 +1100

    Add 'boomsvgloader-sprite' ID to sprite DIV
    Allows to easily find the div using `document.getElementById()`

--- src/js/boomsvgloader.js
+++ src/js/boomsvgloader.js
@@ -21,6 +21,7 @@
 			  return;
       
       var div = document.createElement("div");
+      div.id = 'boomsvgloader-sprite';
       div.style.cssText = 'border: 0; clip: rect(0 0 0 0); height: 0; overflow: hidden; padding: 0; position: absolute; width: 0; opacity: 0;';
       div.innerHTML = ajax.responseText;
       document.body.insertBefore(div, document.body.childNodes[0]);
